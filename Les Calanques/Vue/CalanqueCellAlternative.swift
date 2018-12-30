d4aa4866a1f12397b63a519607e4b3e303e39ec8
Jean Sébastien FRANTZ <rougeterrien@gmail.com>
2018-12-30 19:25:32 +0100

    Logique UITableViewDelegate

---
+++ Les Calanques/Vue/CalanqueCellAlternative.swift
@@ -0,0 +1,36 @@
+//
+//  CalanqueCellAlternative.swift
+//  Les Calanques
+//
+//  Created by seb on 30/12/2018.
+//  Copyright © 2018 seb. All rights reserved.
+//
+
+import UIKit
+
+class CalanqueCellAlternative: UITableViewCell {
+
+    @IBOutlet weak var calanqueIV: UIImageView!
+    
+    @IBOutlet weak var nomLabel: UILabel!
+    
+    var calanque: Calanque? {
+        didSet {
+            if calanque != nil {
+                calanqueIV.image = calanque!.image
+                nomLabel.text = calanque!.nom
+            }
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
