c103475ed594335c55f5f7a7c5001a945244a355
Jean Sébastien FRANTZ <rougeterrien@gmail.com>
2019-01-01 13:59:05 +0100

    UI du controller avec carte

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class ControllerAvecCarte: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func segmentedChange(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func getPosition(_ sender: Any) {
+    }
+  
 }
